aab93cc5772973d5e9b889b6c6faeb063f339686
somayehhosseinbeig <somayeh.hosseinbeig@gmail.com>
2026-05-30 08:25:11 +0330

    Initial commit

---
+++ hello/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world!")
+
+def somayeh(requset):
+    return HttpResponse("Hello Somayeh")
+
+def arnika(requset):
+    return HttpResponse("Hello Arnika!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
